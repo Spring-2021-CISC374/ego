7af52fd8ab0f61186aafed5970ea53b9cd7178cd
Joshua Davis <jhdavis@udel.edu>
2021-05-10 21:51:18 -0400

    Updates

--- beta/sw.js
+++ beta/sw.js
@@ -1,3 +1,5 @@
+importScripts("/ego/betaprecache-manifest.2de2221010bddd81cbc5e8c1f46812c8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 /**
  * You should only modify this, if you know what you are doing.
  * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
@@ -5,7 +7,5 @@
  * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
  * webpack/webpack.common.js
  */
-import { precacheAndRoute } from 'workbox-precaching'
+workbox.precaching.precacheAndRoute(__precacheManifest)
 
-// Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST)
