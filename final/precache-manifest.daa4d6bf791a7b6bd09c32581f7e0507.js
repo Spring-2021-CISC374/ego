e1d1d51874a71ee1a688b5bb3299c1da31ecb425
samvdam <samvd@udel.edu>
2021-05-18 03:43:43 -0400

    Updates

---
+++ final/precache-manifest.daa4d6bf791a7b6bd09c32581f7e0507.js
@@ -0,0 +1,582 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334f2c4fc875df1977e0ab92c7641fdf",
+    "url": "/ego/finalassets/aseprite/Ancient Laser Dude.aseprite"
+  },
+  {
+    "revision": "8617dc280fa924c84c47803c6e8f4928",
+    "url": "/ego/finalassets/img/Bandits/EULA.txt"
+  },
+  {
+    "revision": "f74721b97bbd5d4d2f15e51b601f538c",
+    "url": "/ego/finalassets/img/Bandits/Instructions.txt"
+  },
+  {
+    "revision": "fde38d6f75bfcba6dc1da8d6825d49e9",
+    "url": "/ego/finalassets/img/Bandits/Preview/Preview_HeavyBandit.png"
+  },
+  {
+    "revision": "a0de6300f0bfd38ccfccf6390802e114",
+    "url": "/ego/finalassets/img/Bandits/Preview/Preview_LightBandit.png"
+  },
+  {
+    "revision": "7bebb280e2f550f68a42bc2300f6daaf",
+    "url": "/ego/finalassets/img/Bandits/Sprites/EnvironmentTiles/EnvironmentTiles.png"
+  },
+  {
+    "revision": "181962c916a38c22390b54d09d7e1945",
+    "url": "/ego/finalassets/img/Bandits/Sprites/EnvironmentTiles/Tile_0.png"
+  },
+  {
+    "revision": "beb0556ab16897d885e5287269910eba",
+    "url": "/ego/finalassets/img/Bandits/Sprites/EnvironmentTiles/Tile_1.png"
+  },
+  {
+    "revision": "be929b68bd2a91b8e9f0ed4d8fe95940",
+    "url": "/ego/finalassets/img/Bandits/Sprites/EnvironmentTiles/Tile_2.png"
+  },
+  {
+    "revision": "7786acf95d9fdf72e3f085e1b2478df3",
+    "url": "/ego/finalassets/img/Bandits/Sprites/EnvironmentTiles/Tile_3.png"
+  },
+  {
+    "revision": "3f19891421c618091e103d6d3b12f353",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_0.png"
+  },
+  {
+    "revision": "aeef2fd48f6f9badeba9eddd153117ce",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_1.png"
+  },
+  {
+    "revision": "81bc95e9f25b4d74ad6d6472ff6133be",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_2.png"
+  },
+  {
+    "revision": "8ff2e37ed8dd97c4a55569b483f93335",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_3.png"
+  },
+  {
+    "revision": "87f9cd98a0670f3820f441d795812c44",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_4.png"
+  },
+  {
+    "revision": "dc53513a5363643d7f9789116a5ac261",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_5.png"
+  },
+  {
+    "revision": "025b101f7d5a4cfb9f321df7f060023c",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_6.png"
+  },
+  {
+    "revision": "3285080c2d811e1fdc21e5b1822185f0",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Attack/HeavyBandit_Attack_7.png"
+  },
+  {
+    "revision": "669a9162120a268e7bbe20a1963cbcfd",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Combat Idle/HeavyBandit_CombatIdle_0.png"
+  },
+  {
+    "revision": "bc6c033ff0262b90201e3c30df6aafaf",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Combat Idle/HeavyBandit_CombatIdle_1.png"
+  },
+  {
+    "revision": "19ef36290f4d39b664ea89b645d09ee5",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Combat Idle/HeavyBandit_CombatIdle_2.png"
+  },
+  {
+    "revision": "4c2e90366c2921529e20d7b0420c68d5",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Combat Idle/HeavyBandit_CombatIdle_3.png"
+  },
+  {
+    "revision": "5edf43361af2ae12a47cab45c64315e2",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Death/HeavyBandit_Death_0.png"
+  },
+  {
+    "revision": "84c2c528596354821ae04e3846427b6f",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/HeavyBandit.png"
+  },
+  {
+    "revision": "47beb73eaea2e448a73957b40c63da88",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Hurt/HeavyBandit_Hurt_0.png"
+  },
+  {
+    "revision": "d7ff16a319272ce781cb19f351f0d8ec",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Hurt/HeavyBandit_Hurt_1.png"
+  },
+  {
+    "revision": "bf997bf71d68380fc71df5236f4c445f",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Idle/HeavyBandit_Idle_0.png"
+  },
+  {
+    "revision": "926d464d342a8e1209ba80c2f2419cfd",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Idle/HeavyBandit_Idle_1.png"
+  },
+  {
+    "revision": "5a6bd76d65ab7e8e1d99abb31e7098b9",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Idle/HeavyBandit_Idle_2.png"
+  },
+  {
+    "revision": "7324e1bbe5ab8ddd1ef455847df721a2",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Idle/HeavyBandit_Idle_3.png"
+  },
+  {
+    "revision": "2a003a1ce36ef1d174643180155c97bc",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Jump/HeavyBandit_Jump_0.png"
+  },
+  {
+    "revision": "c26eefcab4852c467d414205d941f6fd",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_0.png"
+  },
+  {
+    "revision": "3c2cc41595757cedf24a1d853a800ee1",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_1.png"
+  },
+  {
+    "revision": "a19404a7787e21baa229609d31ce7f9a",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_2.png"
+  },
+  {
+    "revision": "d3724d485598fc91e30d6e2148046fe1",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_3.png"
+  },
+  {
+    "revision": "30eccf2e3aa02ca63a32358dca4b911c",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_4.png"
+  },
+  {
+    "revision": "2fb776e787d723bc70c985d025d6359d",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_5.png"
+  },
+  {
+    "revision": "f93dd3528cafa86a5786d5773dbdbd58",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_6.png"
+  },
+  {
+    "revision": "3cbe37a358cac23d6f3591717f96ae53",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Recover/HeavyBandit_Recover_7.png"
+  },
+  {
+    "revision": "0da31ef2da6541a4c64b800c1fe131af",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_0.png"
+  },
+  {
+    "revision": "d580d8f17e07ca5fdb3e559f049d91a6",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_1.png"
+  },
+  {
+    "revision": "c875930b511a52dfb3d9dd6e622c4d4d",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_2.png"
+  },
+  {
+    "revision": "539e40a26152554d6ba2036d0b735d0b",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_3.png"
+  },
+  {
+    "revision": "250968e1a26ab72162efb4c5c7560c75",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_4.png"
+  },
+  {
+    "revision": "10329095e5cacf2d3a3e5734c5cfb58a",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_5.png"
+  },
+  {
+    "revision": "26da6ace48a24d5b48f3a49cce7ce2c8",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_6.png"
+  },
+  {
+    "revision": "ec6c148f1e4b754bbaa518ae85c2688f",
+    "url": "/ego/finalassets/img/Bandits/Sprites/Heavy Bandit/Run/HeavyBandit_Run_7.png"
+  },
+  {
+    "revision": "c5cd8eeac56be144eb37327862458310",
+    "url": "/ego/finalassets/img/Cards/egoCard-1.png"
+  },
+  {
+    "revision": "8170e631a00318135c4f1e089e8524d6",
+    "url": "/ego/finalassets/img/Cards/egoCard-10.png"
+  },
+  {
+    "revision": "0e7b7b3ee9676a322b6caeceae7c1517",
+    "url": "/ego/finalassets/img/Cards/egoCard-11.png"
+  },
+  {
+    "revision": "d925bd768f2680e82b13dfcc03fd78f5",
+    "url": "/ego/finalassets/img/Cards/egoCard-12.png"
+  },
+  {
+    "revision": "b71588577922ef3a84e3ab623d2c646d",
+    "url": "/ego/finalassets/img/Cards/egoCard-13.png"
+  },
+  {
+    "revision": "54de812bc0b2747263107ecde286b330",
+    "url": "/ego/finalassets/img/Cards/egoCard-14.png"
+  },
+  {
+    "revision": "f929760c5bcf73f68bea228aba0b5e69",
+    "url": "/ego/finalassets/img/Cards/egoCard-15.png"
+  },
+  {
+    "revision": "73c8c022bbf05ea9264b7e846d0cf8d3",
+    "url": "/ego/finalassets/img/Cards/egoCard-16.png"
+  },
+  {
+    "revision": "8ffb167a385e083ceb3cf7fba1f21eb1",
+    "url": "/ego/finalassets/img/Cards/egoCard-17.png"
+  },
+  {
+    "revision": "0f9668b36d4b23ab4d9c0970a411947d",
+    "url": "/ego/finalassets/img/Cards/egoCard-18.png"
+  },
+  {
+    "revision": "9488f78f8ebc85721a7c031eebb3d54c",
+    "url": "/ego/finalassets/img/Cards/egoCard-2.png"
+  },
+  {
+    "revision": "2b20c7b62e149d570c356c52e68e7a1a",
+    "url": "/ego/finalassets/img/Cards/egoCard-3.png"
+  },
+  {
+    "revision": "54770856531931c54cf38c6e39700328",
+    "url": "/ego/finalassets/img/Cards/egoCard-4.png"
+  },
+  {
+    "revision": "a85149a8965561124d66df2a1e2630d8",
+    "url": "/ego/finalassets/img/Cards/egoCard-5.png"
+  },
+  {
+    "revision": "ce3d814837215c1bf66a70ddfa84d3e6",
+    "url": "/ego/finalassets/img/Cards/egoCard-6.png"
+  },
+  {
+    "revision": "e7ab32827942fef41e6840282b05d14a",
+    "url": "/ego/finalassets/img/Cards/egoCard-7.png"
+  },
+  {
+    "revision": "0bdb7db9a082ae8eaedbd16aa009162f",
+    "url": "/ego/finalassets/img/Cards/egoCard-8.png"
+  },
+  {
+    "revision": "8cc18131b90576723f3ccbcf10b8f914",
+    "url": "/ego/finalassets/img/Cards/egoCard-9.png"
+  },
+  {
+    "revision": "0347a5ddaa688d99a996af41ef8d3a4f",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_0.png"
+  },
+  {
+    "revision": "ea2f0b564f0dfc1841285ba8f950559d",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_1.png"
+  },
+  {
+    "revision": "64a50786fadf724165d198c3b21f419a",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_2.png"
+  },
+  {
+    "revision": "592aba605edbee851e6515c9cf173689",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_3.png"
+  },
+  {
+    "revision": "4274e14b0203ccfcd4d4a84881a932d6",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_4.png"
+  },
+  {
+    "revision": "1add453d014786dd984081da8dd00d83",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_5.png"
+  },
+  {
+    "revision": "b89e137062a7c0db395ad16dd6f12e55",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_6.png"
+  },
+  {
+    "revision": "f51c0ec78cd0e41d7c9f5319de6f205b",
+    "url": "/ego/finalassets/img/Light Bandit/Attack/LightBandit_Attack_7.png"
+  },
+  {
+    "revision": "cc1af6e5206f86b57ec5d113a4ec2d3b",
+    "url": "/ego/finalassets/img/Light Bandit/Combat Idle/LightBandit_Combat Idle_0.png"
+  },
+  {
+    "revision": "0f275a10b223d42703e10bc73bf09f48",
+    "url": "/ego/finalassets/img/Light Bandit/Combat Idle/LightBandit_Combat Idle_1.png"
+  },
+  {
+    "revision": "a05cc99dd38252bdb9d378ebdf31ba2f",
+    "url": "/ego/finalassets/img/Light Bandit/Combat Idle/LightBandit_Combat Idle_2.png"
+  },
+  {
+    "revision": "e6ae0bacc7474b5b98f51eaab8535770",
+    "url": "/ego/finalassets/img/Light Bandit/Combat Idle/LightBandit_Combat Idle_3.png"
+  },
+  {
+    "revision": "a8f6a504a473f5f5ed784eacee54fb2b",
+    "url": "/ego/finalassets/img/Light Bandit/Death/LightBandit_Death_0.png"
+  },
+  {
+    "revision": "725f8c2e890165afba8181f8ad3e5118",
+    "url": "/ego/finalassets/img/Light Bandit/Hurt/LightBandit_Hurt_0.png"
+  },
+  {
+    "revision": "41ad3aaa6fe5f0fdae6af118da888c74",
+    "url": "/ego/finalassets/img/Light Bandit/Hurt/LightBandit_Hurt_1.png"
+  },
+  {
+    "revision": "1943bc7988e922499f7f22e85d636ea9",
+    "url": "/ego/finalassets/img/Light Bandit/Idle/LightBandit_Idle_0.png"
+  },
+  {
+    "revision": "8798e95fb2be108800dcc11704f8551a",
+    "url": "/ego/finalassets/img/Light Bandit/Idle/LightBandit_Idle_1.png"
+  },
+  {
+    "revision": "7a09857ec6d641860e9a26ef42e7770d",
+    "url": "/ego/finalassets/img/Light Bandit/Idle/LightBandit_Idle_2.png"
+  },
+  {
+    "revision": "7cf1bc4036bfdd1daedbbdc457b0212b",
+    "url": "/ego/finalassets/img/Light Bandit/Idle/LightBandit_Idle_3.png"
+  },
+  {
+    "revision": "0579609d1d7d07dd031d0e4cfdd91d92",
+    "url": "/ego/finalassets/img/Light Bandit/Jump/LightBandit_Jump_0.png"
+  },
+  {
+    "revision": "92eea30ffb2962262c06da5d4518c3e8",
+    "url": "/ego/finalassets/img/Light Bandit/LightBandit.png"
+  },
+  {
+    "revision": "56ed31729d9c26428d333d8669b5b2b3",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_0.png"
+  },
+  {
+    "revision": "e3eae716fd343efece89756f7bf59721",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_1.png"
+  },
+  {
+    "revision": "22ecea3b15ef7e0dc4b64e39be21af85",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_2.png"
+  },
+  {
+    "revision": "7dc0d1888f7b6a9bc4ced0528746d1cc",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_3.png"
+  },
+  {
+    "revision": "c4b13f43462f3b3bb7278a62e22471d0",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_4.png"
+  },
+  {
+    "revision": "a29058e9f1f60b8391e2890f3875db36",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_5.png"
+  },
+  {
+    "revision": "639a92d981e08d13a87412e57e4deb83",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_6.png"
+  },
+  {
+    "revision": "826b809eaf54aa16320731c00f031d7d",
+    "url": "/ego/finalassets/img/Light Bandit/Recover/LightBandit_Recover_7.png"
+  },
+  {
+    "revision": "c119a82cdc65061f32c4331931c1ae32",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_0.png"
+  },
+  {
+    "revision": "e3438f845d6b1c861770ea963fdfbd15",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_1.png"
+  },
+  {
+    "revision": "54ba2873efa30191565301a5dac51657",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_2.png"
+  },
+  {
+    "revision": "7a78e01b85f11a4fee024abba697fbe2",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_3.png"
+  },
+  {
+    "revision": "132b1ce70b8cd25bc34d76d5557ebc97",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_4.png"
+  },
+  {
+    "revision": "953d7a405a95e70d41756ecbe3bc0066",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_5.png"
+  },
+  {
+    "revision": "4b84906a619159a7c6242416a62aa0c8",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_6.png"
+  },
+  {
+    "revision": "1c43d221f70d2cd4801de1d4ff47aa4e",
+    "url": "/ego/finalassets/img/Light Bandit/Run/LightBandit_Run_7.png"
+  },
+  {
+    "revision": "91d189d6788fdbd65dd17c5ccb63283a",
+    "url": "/ego/finalassets/img/NightBorne/NightBorne.png"
+  },
+  {
+    "revision": "260387402dafda3fb74933446dd2b2d0",
+    "url": "/ego/finalassets/img/NightBorne/NightBorne_attack.gif"
+  },
+  {
+    "revision": "630c1de86f8de4c86ea2fc55f1751875",
+    "url": "/ego/finalassets/img/NightBorne/NightBorne_death..gif"
+  },
+  {
+    "revision": "dbafe69eceb24b3ece22d438116799dd",
+    "url": "/ego/finalassets/img/NightBorne/NightBorne_hurt.gif"
+  },
+  {
+    "revision": "893e9304df6b9d9da434d5637265837a",
+    "url": "/ego/finalassets/img/NightBorne/NightBorne_idle.gif"
+  },
+  {
+    "revision": "64613dae7f80bc6895720af744d8f75f",
+    "url": "/ego/finalassets/img/NightBorne/NightBorne_run.gif"
+  },
+  {
+    "revision": "95e518cf981ea86c95c4e0a4764663e3",
+    "url": "/ego/finalassets/img/background.png"
+  },
+  {
+    "revision": "95e518cf981ea86c95c4e0a4764663e3",
+    "url": "/ego/finalassets/img/backgroundWood.png"
+  },
+  {
+    "revision": "c16be5473fba352835672a655b7ac81c",
+    "url": "/ego/finalassets/img/char2/spriteEast1.png"
+  },
+  {
+    "revision": "49f30d5add42b6b70f7edce974f3ef8a",
+    "url": "/ego/finalassets/img/char2/spriteNorth1.png"
+  },
+  {
+    "revision": "337bd22ec16007244dce931f17b31356",
+    "url": "/ego/finalassets/img/char2/spriteSouth1.png"
+  },
+  {
+    "revision": "a89fb1aabbd1d3f25a72e7e4af12b1f6",
+    "url": "/ego/finalassets/img/char2/spriteWest1.png"
+  },
+  {
+    "revision": "995626182c8120acef119510df0e1a74",
+    "url": "/ego/finalassets/img/char3/spriteEast1.png"
+  },
+  {
+    "revision": "262a28d759bac20e54e7c264ddd2a9a6",
+    "url": "/ego/finalassets/img/char3/spriteNorth1.png"
+  },
+  {
+    "revision": "d2c4db9d171a344b5929114c0a1b73ca",
+    "url": "/ego/finalassets/img/char3/spriteSouth1.png"
+  },
+  {
+    "revision": "09c6d4f63590cef00367e7c35abd1c4b",
+    "url": "/ego/finalassets/img/char3/spriteWest1.png"
+  },
+  {
+    "revision": "67b04700544b00d5556c2eb7d7148bad",
+    "url": "/ego/finalassets/img/charSpriteEast.png"
+  },
+  {
+    "revision": "e6b6dab62019fb1f6ef3a72321759529",
+    "url": "/ego/finalassets/img/charSpriteNorth.png"
+  },
+  {
+    "revision": "54cf9b4f0d644fe69afc91fa0e525974",
+    "url": "/ego/finalassets/img/charSpriteSouth.png"
+  },
+  {
+    "revision": "161ddfc1d1a0e5583315e95348da3508",
+    "url": "/ego/finalassets/img/charSpriteWest.png"
+  },
+  {
+    "revision": "1855cf4e30ae4240d571605fc4ce0eab",
+    "url": "/ego/finalassets/img/clouds/largeCloud.png"
+  },
+  {
+    "revision": "870fc7d85be48f00b92100aa8f781fa3",
+    "url": "/ego/finalassets/img/clouds/mediumCloud.png"
+  },
+  {
+    "revision": "95e4981ee4c49d9c0abebdef9bcb5341",
+    "url": "/ego/finalassets/img/clouds/smallCloud.png"
+  },
+  {
+    "revision": "d0753cebf36d98cf7c10318ac0c307f0",
+    "url": "/ego/finalassets/img/ego-main-screen.jpg"
+  },
+  {
+    "revision": "91ef6aa23b8fd33bb75bb828ab2e7b21",
+    "url": "/ego/finalassets/img/egoEnemies-01.png"
+  },
+  {
+    "revision": "499346942a791600a271f282122df2df",
+    "url": "/ego/finalassets/img/egoEnemies-02.png"
+  },
+  {
+    "revision": "652e31195504b54ba3a010f6fec82ab3",
+    "url": "/ego/finalassets/img/egoEnemies-03.png"
+  },
+  {
+    "revision": "ff0942bbd7130173a08e9eb5d6cf3760",
+    "url": "/ego/finalassets/img/egoLogo.png"
+  },
+  {
+    "revision": "c3e8c9af6624dc1fe76adbee9177562c",
+    "url": "/ego/finalassets/img/elderSprite.png"
+  },
+  {
+    "revision": "75c9d53d84e19c679bdbf42e15839c1e",
+    "url": "/ego/finalassets/img/house.png"
+  },
+  {
+    "revision": "69f51ef3e2f0ef6934bc31d11ed7d231",
+    "url": "/ego/finalassets/img/overworld/grassTexture.png"
+  },
+  {
+    "revision": "10ebd0cf338564d5ba75a6db9933bef6",
+    "url": "/ego/finalassets/img/overworld/houseLarge.png"
+  },
+  {
+    "revision": "e955e195cf6cad806df1b3ddc427a212",
+    "url": "/ego/finalassets/img/overworld/houseMedium.png"
+  },
+  {
+    "revision": "5129cfb9b020ffc32333ddfab954019b",
+    "url": "/ego/finalassets/img/overworld/houseSmall.png"
+  },
+  {
+    "revision": "18a5c35040e40b577692c19ed10d826f",
+    "url": "/ego/finalassets/img/overworld/indoorTexture.png"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "/ego/finalassets/img/phaser-logo.png"
+  },
+  {
+    "revision": "7466529c2d9db10244f46e534d04187f",
+    "url": "/ego/finalassets/img/stars.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "/ego/finalfavicon.ico"
+  },
+  {
+    "revision": "fc90bfd78c9cfdf73aeb",
+    "url": "/ego/finalgen.main.56e0beb2fd444573400e.bundle.js"
+  },
+  {
+    "revision": "1ee88727db96e1d99f6f",
+    "url": "/ego/finalgen.vendors.f7966289ef0034661003.bundle.js"
+  },
+  {
+    "revision": "e2587028c660f8e409d3104f2829dbe8",
+    "url": "/ego/finalindex.html"
+  },
+  {
+    "revision": "fb569f100bc37dea9198d1243c879c06",
+    "url": "/ego/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/ego/finalsw.js"
+  }
+]);
